421dcb2575f9ea9220502d3e9a465d251a0d72e6
008ken <43024052+008ken@users.noreply.github.com>
2019-04-25 00:01:20 +0800

    添加静态文件 面包屑 头部底部

---
+++ src/views/GoodsList.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <nav-header></nav-header>
+    <nav-bread>
+      <span slot="bread">Goods</span>
+    </nav-bread>
+    <nav-footer></nav-footer>
+  </div>
+</template>
+<script>
+  import './../assets/css/base.css'
+  import './../assets/css/product.css'
+  import NavHeader from './../components/NavHeader.vue'
+  import NavFooter from './../components/NavFooter.vue'
+  import NavBread from './../components/NavBread.vue'
+
+  export default {
+    data() {
+      return {msg: 'hello vue'}
+    },
+    components: {
+      NavHeader: NavHeader,
+      NavFooter: NavFooter,
+      NavBread: NavBread
+    }
+    //components: {'other-component': OtherComponent, HeaderComponent,}
+  }</script>
